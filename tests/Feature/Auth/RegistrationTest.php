f3ac76530f46f69ffd14c223a79e74ad21597ba2
Katon Surya Atmaja <katonatmaja@gmail.com>
2026-01-02 20:49:41 +0700

    Update test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -8,7 +8,8 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $response = $this->post(route('register.store'), [
-        'name' => 'Test User',
+        'public_handle' => 'filledForTestAndShouldBeRandomized',
+        'display_name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
