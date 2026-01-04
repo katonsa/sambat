b2701c81c2a1a51527b2417f3fe67e131c840a27
Katon Surya Atmaja <katonatmaja@gmail.com>
2026-01-04 20:13:40 +0700

    remove some laravel boilerplate

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,24 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('feed', function () {
+        return Inertia::render('feed');
+    })->name('feed');
+
     Route::get('users/{user:public_handle}', [UserController::class, 'show'])->name('users.show');
+
+    Route::get('me', function () {
+        // Redirect to users/{current_user_public_handle}
+        return redirect()->route('users.show', auth()->user()->public_handle);
+    });
+
 });
 
 require __DIR__ . '/settings.php';
